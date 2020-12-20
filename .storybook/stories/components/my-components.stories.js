fd5d7ce1e54e612cf1a3a5e045635aef11064cb6
contiki9 <contiki9@gmail.com>
2020-12-20 12:43:50 +0900

    👍 storybook -> addonの追加

--- .storybook/stories/components/my-components.stories.js
+++ .storybook/stories/components/my-components.stories.js
@@ -1,6 +1,7 @@
 import { storiesOf } from '@storybook/html';
+import notes from '../../../src/components/my-component/readme.md';
 
 storiesOf('components', module)
   .add('sample', () => `
   <my-component first="Stencil" last="'Don't call me a framework' JS"></my-component>
-  `)
+  `, { notes })
